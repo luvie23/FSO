0ace8a1a645bbfb4b8042ab964882bfc7cb29746
luvie23 <luvierallos@gmail.com>
2022-11-21 17:27:44 +0800

    finish exercise 1.10

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -13,6 +13,12 @@ const Header = (props) => {
   )
 }
 
+const StatisticLine = (props) => {
+  return(
+  <p>{props.text} {props.value}</p>
+  )
+}
+
 const Statistics = (props) => {
   if (props.all === 0){
     return (
@@ -20,8 +26,14 @@ const Statistics = (props) => {
     )
   }
   return(
-    <p>good {props.good}<br/>neutral {props.neutral}<br/> bad {props.bad}<br/> all {props.all}<br/> average {props.average}<br/> positive {props.positive}</p>
-    
+    <div>
+      <StatisticLine text='good' value={props.good}/>
+      <StatisticLine text='neutral' value={props.neutral}/>
+      <StatisticLine text='bad' value={props.bad}/>
+      <StatisticLine text='all' value={props.all}/>
+      <StatisticLine text='average' value={props.average}/>
+      <StatisticLine text='positive' value={props.positive}/>
+    </div>
   )
 }
 
